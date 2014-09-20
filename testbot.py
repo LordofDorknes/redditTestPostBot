8b37f77fc13fbc4f1f19322e68518aaff23070dc
LordofDorknes <rcn94087@gmail.com>
2014-09-19 23:52:37 -0700

    lel

--- testbot.py
+++ testbot.py
@@ -1,4 +1,4 @@
 import praw
-
-user_agent = ("Some Testing Bot by /u/LordofDorknes"
+user_agent = ("Some Testing Bot v 0.0.1 by /u/LordofDorknes"
 ...               "github.com/LordofDorknes/redditTestPostBot")
+r = praw.Reddit(user_agent=user_agent)
