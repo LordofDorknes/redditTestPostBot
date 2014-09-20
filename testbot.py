ce6b5a927c401cb56bff87cbb6de761038c66782
LordofDorknes <rcn94087@gmail.com>
2014-09-20 00:12:21 -0700

    :l

--- testbot.py
+++ testbot.py
@@ -1,4 +1,6 @@
+import time
 import praw
 user_agent = ("Some Testing Bot v 0.0.1 by /u/LordofDorknes"
-...               "github.com/LordofDorknes/redditTestPostBot")
+              "github.com/LordofDorknes/redditTestPostBot")
 r = praw.Reddit(user_agent=user_agent)
+r.login
