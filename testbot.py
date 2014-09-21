11d08f6f989a22a4cdc6476cb7e148796118e559
LordofDorknes <rcn94087@gmail.com>
2014-09-20 19:49:01 -0700

    Added Functionality
    It does things now. This should post to reddit/r/test when I give it the
    password

--- testbot.py
+++ testbot.py
@@ -1,6 +1,14 @@
-import time
-import praw
+import time # unnecessary import
+import praw #import reddit api
+password = "Password" """password variable for login,
+TODO: make it load from cofig"""
+
 user_agent = ("Some Testing Bot v 0.0.1 by /u/LordofDorknes"
               "github.com/LordofDorknes/redditTestPostBot")
+# define unique useragent
+
 r = praw.Reddit(user_agent=user_agent)
-r.login
+r.login('TestingBotAccount1', password) #log in
+PostTitle = 'Test Post 1'
+PostText = 'Test post among many others'
+r.submit('test',PostTitle,text=PostText)
